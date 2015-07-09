a479a4c1bb1be099020b46343d3ff4421980900a
aexpositog <aexpositog@gmail.com>
2015-07-09 17:16:48 +0200

    Autoload

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,21 @@ var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
+//Carga la página de inicio
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+//Autolad de comandos con :quizId
+router.param('quizId', quizController.load);  //autoload :quizId
+
 // Definición de rutas de /quizes
+// Controladores para preguntas y respuestas
 router.get('/quizes',						quizController.index);
 router.get('/quizes/:quizId(\\d+)',			quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer',	quizController.answer);
+
+// Carga la página de Créditos o Autor
 router.get('/author', quizController.author);
 
 module.exports = router;
