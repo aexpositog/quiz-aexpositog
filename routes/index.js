aecea01dcc2cd26e7e6740f1c1c5effa053411f5
aexpositog <aexpositog@gmail.com>
2015-07-28 13:28:46 +0200

    Crear Comentario

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
+var commentController = require('../controllers/comment_controller');
 
 /* GET home page. */
 //Carga la página de inicio
@@ -22,6 +23,9 @@ router.post('/quizes/create',				quizController.create);
 router.get('/quizes/:quizId(\\d+)/edit',	quizController.edit);
 router.put('/quizes/:quizId(\\d+)',			quizController.update);
 router.delete('/quizes/:quizId(\\d+)',		quizController.destroy);
+
+router.get('/quizes/:quizId(\\d+)/comments/new',	commentController.new);
+router.post('/quizes/:quizId(\\d+)/comments',		commentController.create);
 
 // Carga la página de Créditos o Autor
 router.get('/author', quizController.author);
