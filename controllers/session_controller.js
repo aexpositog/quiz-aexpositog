592402b8db5f2a84380bd633f6f1d1447f6dcc19
aexpositog <aexpositog@gmail.com>
2015-07-30 19:02:00 +0200

    Auto-logout de sesión v.2

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -19,9 +19,6 @@ exports.create = function(req, res) {
 	var login		= req.body.login;
 	var password	= req.body.password;
 
-	// variable de sesion donde se guarda la hora de la ultima actividad
-	req.session.horaUltimoAcceso = (new Date()).getTime();
-
 	var userController = require('./user_controller');
 	userController.autenticar(login, password, function(error, user){
 		if(error){  // si hay error retornamos mensajes de error de sesión
@@ -32,7 +29,7 @@ exports.create = function(req, res) {
 
 		// Crear req.session.user y guardar campos id y username
 		// La sesión se define por la existencia de: req.session.user
-		req.session.user = {id:user.id, username:user.username};
+		req.session.user = {id:user.id, username:user.username, lastRequestTime: Date.now()};
 		res.redirect(req.session.redir.toString());// redirección a path anterior al login
 	});
 };
